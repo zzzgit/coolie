2d58cd6addf01bf11d08a8e637c71c816ab2fd66
jonkee <zzzgoo@gmail.com>
2018-09-10 22:48:45 +0800

    caogao

---
+++ index.js
@@ -0,0 +1,13 @@
+class Coolie {
+    constructor(setting) {
+ 
+    },
+    static type = {
+        instant: 1,
+        timing: 2,
+        internal: 3,
+        manual:4
+    }
+}
+
+module.exports = Coolie
