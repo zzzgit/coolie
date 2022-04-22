1e8cb916a12055254081cf8b0068d66be7061964
jonkee <zzzgoo@gmail.com>
2022-04-22 21:57:20 +0800

    chore: sslls

--- src/test.ts
+++ src/test.ts
@@ -28,7 +28,7 @@ const kkkkkkk = (that: ISubscribable, init: number): Promise<number> => {
 	if (al > 10) {
 		return Promise.resolve(al + 2)
 	}
-	return Promise.reject(new Error("error:" + al))
+	return Promise.reject(new Error("er2ror:" + al))
 }
 
 const t = new Task(kkkkkkk)
