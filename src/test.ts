286feb2075027d0f6610a66c39bcf90da5c2b053
jonkee <zzzgoo@gmail.com>
2022-04-22 21:38:50 +0800

    ssk

--- src/test.ts
+++ src/test.ts
@@ -28,7 +28,7 @@ const kkkkkkk = (that: ISubscribable, init: number): Promise<number> => {
 	if (al > 10) {
 		return Promise.resolve(al + 2)
 	}
-	return Promise.reject(new Error("e3rror:" + al))
+	return Promise.reject(new Error("error:" + al))
 }
 
 const t = new Task(kkkkkkk)
